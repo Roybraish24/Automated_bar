9edd7c5a9ea1390ef80d36a6fef712e7a57036f2
Roy Braish <roy.braish@neo.ae>
2023-01-20 21:53:56 +0200

    Update back button [Code-update]

--- AutomatedBar/app/src/main/java/com/example/automatedbar/MainActivity.java
+++ AutomatedBar/app/src/main/java/com/example/automatedbar/MainActivity.java
@@ -2,14 +2,9 @@ package com.example.automatedbar;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.bluetooth.BluetoothAdapter;
-import android.bluetooth.BluetoothDevice;
-import android.bluetooth.BluetoothManager;
 import android.content.Intent;
 import android.os.Bundle;
-import android.os.Handler;
 
-import java.util.Set;
 
 public class MainActivity extends AppCompatActivity {
 
