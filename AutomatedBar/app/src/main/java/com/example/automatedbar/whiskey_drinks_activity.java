68c0f78c8222656daceda81d1057b8398e0bb3a4
Roy Braish <roy.braish@neo.ae>
2023-01-20 21:56:03 +0200

    Create dropdown whiskey choose drink [Code-creation]

--- AutomatedBar/app/src/main/java/com/example/automatedbar/whiskey_drinks_activity.java
+++ AutomatedBar/app/src/main/java/com/example/automatedbar/whiskey_drinks_activity.java
@@ -1,11 +1,14 @@
 package com.example.automatedbar;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.graphics.Color;
 import android.graphics.Typeface;
+
 import android.os.Bundle;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Spinner;
@@ -14,58 +17,80 @@ import android.widget.TextView;
 public class whiskey_drinks_activity extends AppCompatActivity {
 
     @Override
+
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_whiskey_drinks);
         Spinner spinner = findViewById(R.id.spinner);
 
         String[] arrayList = getResources().getStringArray(R.array.whiskey_drinks);
-        ArrayAdapter<String> spinnerArrayAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, arrayList);
-        spinnerArrayAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
 
-        spinner.setAdapter(spinnerArrayAdapter);
-        spinner.setSelection(0, true);
-        TextView spinnerText = (TextView) spinner.getSelectedView();
-        spinnerText.setTypeface(spinnerText.getTypeface(), Typeface.BOLD);
+        ArrayAdapter<String> spinnerArrayAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, arrayList) {
+            @Override
+            public boolean isEnabled(int position) {
+                // Disable the first item from Spinner
+                // First item will be use for hint
+                return position != 0;
+            }
 
+            @Override
+            public View getDropDownView(
+                    int position, View convertView,
+                    @NonNull ViewGroup parent) {
+
+                // Get the item view
+                View view = super.getDropDownView(
+                        position, convertView, parent);
+                TextView textView = (TextView) view;
+                if (position == 0) {
+                    // Set the hint text color gray
+                    textView.setTextColor(Color.GRAY);
+                } else {
+                    textView.setTextColor(Color.BLACK);
+                }
+                return view;
+            }
+        };
 //
-//        Spinner spinner = (Spinner) findViewById(R.id.spinner);
-//// Create an ArrayAdapter using the string array and a default spinner layout
-//        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
-//                R.array.whiskey_drinks, android.R.layout.simple_spinner_item);
-//// Specify the layout to use when the list of choices appears
-//        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-//// Apply the adapter to the spinner
-//        spinner.setAdapter(adapter);
+        spinner.setOnItemSelectedListener(
+                new AdapterView.OnItemSelectedListener() {
+
+                    @Override
+                    public void onItemSelected(
+                            AdapterView<?> parent, View view,
+                            int position, long id) {
+
+                        // Get the spinner selected item text
+                        String selectedItemText = (String) parent
+                                .getItemAtPosition(position);
+                        TextView spinnerText = (TextView) spinner.getSelectedView();
+                        spinnerText.setTypeface(spinnerText.getTypeface(), Typeface.BOLD);
+                        getDrawable(R.drawable.spinner_background);
 
 
-//        spinner.setBackgroundColor(Color.WHITE);
 
-        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
-            @Override
-            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
-                String selectedItem = parent.getItemAtPosition(position).toString();
-                // perform check or action based on selected item
-                TextView spinnerText = (TextView) spinner.getSelectedView();
-                spinnerText.setTypeface(spinnerText.getTypeface(), Typeface.BOLD);
+                        // If user change the default selection
+                        // First item is disable and
+                        // it is used for hint
+                    }
 
-                spinner.setBackgroundColor(Color.GREEN);
-            }
+                    @Override
+                    public void onNothingSelected(
+                            AdapterView<?> parent) {
+                    }
+                });
 
-            @Override
-            public void onNothingSelected(AdapterView<?> parent) {
-//                TextView spinnerText = (TextView) spinner.getSelectedView();
-//                spinnerText.setTypeface(spinnerText.getTypeface(), Typeface.BOLD);
 
-//                spinner.setBackgroundColor(Color.WHITE);
-
-            }
-        });
+        // Finally, data bind the spinner object with adapter
+        spinner.setAdapter(spinnerArrayAdapter);
     }
 
-    public void back_to_dashboard(View view){
 
+    public void back_to_dashboard(View view) {
+        finish();
     }
+}
+
 //    public void send_bluetooth_message(View view){
 //    }
 //
@@ -73,4 +98,3 @@ public class whiskey_drinks_activity extends AppCompatActivity {
 //
 //    }
 
-}
